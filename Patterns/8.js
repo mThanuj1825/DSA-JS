7af76ed579ba4d7318d32b5091fae07be1b623ef
mThanuj <thanujmullaguri1825@gmail.com>
2024-06-01 19:55:30 +0530

    mThanuj

---
+++ Patterns/8.js
@@ -0,0 +1,15 @@
+let n = 5;
+
+for (let i = 0; i < n; i++) {
+    let row = "";
+
+    for (let j = 0; j < i; j++) {
+        row += "  ";
+    }
+
+    for (let j = (2 * (n - i) - 1); j > 0; j--) {
+        row += "* ";
+    }
+
+    console.log(row);
+}
